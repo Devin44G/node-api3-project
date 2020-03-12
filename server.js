2f91387b7ed4aa4b3d6aba5423306445e1e05315
Devin44G <35251347+Devin44G@users.noreply.github.com>
2020-03-12 09:42:20 -0500

    Logger middleware written -> 1/4 Completed

--- server.js
+++ server.js
@@ -2,12 +2,20 @@ const express = require('express');
 
 const server = express();
 
+server.use(logger);
+
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  const method = req.method;
+  const endpoint = req.originalUrl;
+
+  console.log(`${method} to ${endpoint}`);
+  next();
+}
 
 module.exports = server;
